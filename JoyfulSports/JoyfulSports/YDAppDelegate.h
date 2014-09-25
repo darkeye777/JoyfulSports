b27cf17d1cc4f1d267646d5176975291400b5592
Zhan <tz46546@163.com>
2014-09-25 20:38:13 +0800

    新建工程

---
+++ JoyfulSports/JoyfulSports/YDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YDAppDelegate.h
+//  JoyfulSports
+//
+//  Created by Tzhan on 14-9-25.
+//  Copyright (c) 2014年 Banana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JoyfulSports/JoyfulSports/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JoyfulSports
+//
+//  Created by Tzhan on 14-9-25.
+//  Copyright (c) 2014年 Banana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YDAppDelegate class]));
+    }
+}
